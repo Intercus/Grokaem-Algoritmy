1177aabcbb2136be718bc2fb12a1fce935783960
Intercrus <67190583+Intercus@users.noreply.github.com>
2024-06-15 19:24:08 +0300

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -6,7 +6,7 @@ def binary_search(list,item):
     low = 0  #low = list[0]
     high = len(list) - 1
     while low <= high:
-        mid = (low + high)
+        mid = (low + high) / 2
         guess = list[mid]
         if guess == item:
             return mid
