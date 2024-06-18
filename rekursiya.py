82f58bbf1bb87989a2d42e898abf82de1d0dc8c0
Intercrus <darsik02@mail.ru>
2024-06-18 13:13:17 +0300

    update

--- rekursiya.py
+++ rekursiya.py
@@ -7,6 +7,7 @@
 #Наблюдение
 #Если будет рекурсия без условия остановки то python не даст выполнить 
 #С ошибкой:maximum recursion depth exceeded(превышена максимальная глубина рекурсии)
+#Факториал числа с помощью рекурсии
 def fact(x):
     if x == 0:
         return 1
@@ -14,3 +15,35 @@ def fact(x):
         print(x)
         return x * fact(x-1)
 print(fact(5))
+
+#Подсчет суммы рекурсией
+def sum_recursive(lst):
+    if len(lst) == 0: #Проверка если список пуст
+        return 0
+    else:
+        return lst[0] + sum_recursive(lst[1:])
+
+# Пример использования
+my_list = [1, 2, 3, 4, 5]
+print(sum_recursive(my_list))  # Вывод: 15
+
+#Подсчет количества элементов в списке
+def kol_element(lst):
+    if not lst: #Проверка если список пуст
+        return 0
+    return 1 + kol_element(lst[1:])
+
+# Пример использования
+my_list = [1, 2, 3, 4, 5]
+print(kol_element(my_list))  # Вывод: 5
+
+#Поиск большего числа в списке
+def max_number(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        max_of_rest = max_number(lst[1:])
+        return lst[0] if lst[0] > max_of_rest else max_of_rest
+
+my_list = [7,8,6,3,4,5]
+print(max_number(my_list))# Вывод: 8
